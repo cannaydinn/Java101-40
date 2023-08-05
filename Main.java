56eadee46b3e3719213168677f9b2082aef479ef
cannaydinn <canaydinn6@gmail.com>
2023-08-05 18:15:34 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] dizi = new String[7][4];
+
+        for(int i = 0; i<dizi.length;i++){
+            for(int j=0;j<dizi[i].length;j++){
+                if((i==0 && j==3) || (i==3 && j==3) || (i==6 && j==3) ){
+                    dizi[i][j]="  ";
+                }
+                else if(i==0  || i==3 || i==6){
+                    dizi[i][j]=" * ";
+                }
+                else if(j==0 || j==3){
+                    dizi[i][j]=" * ";
+                }
+                else{
+                    dizi[i][j]="  ";
+                }
+            }
+        }
+
+        for(String[] row : dizi ){
+            for(String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
